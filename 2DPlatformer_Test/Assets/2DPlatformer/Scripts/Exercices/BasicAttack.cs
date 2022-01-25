75a944fc26cb041e03745eac4cf865fe7a323ee2
Kywertz <mz>
2022-01-25 16:32:15 +0100

    ADD BasicAttack.cs

---
+++ 2DPlatformer_Test/Assets/2DPlatformer/Scripts/Exercices/BasicAttack.cs
@@ -0,0 +1,101 @@
+namespace GSGD2.Gameplay
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using GSGD2.Player;
+    using UnityEngine.InputSystem;
+
+    public class BasicAttack : MonoBehaviour
+    {
+
+
+        [SerializeField]
+        private GameObject _fluteattackPrefab = null;
+
+        private bool _startdown = false;
+
+        private float _cooldown = 1f;
+
+        [SerializeField]
+        private float _bigfloat = 2f;
+
+        [SerializeField]
+        private InteractWithDamageable _projectileInteractWith = 0f;
+
+        [SerializeField]
+        private InteractWithDamageable _damageDealerInteractWith = 0f;
+
+        private InputAction _inputaction = null;
+
+        [SerializeField]
+        private InputActionMapWrapper _inputActionmap = null;
+
+        private bool _starttimer = false;
+
+        [SerializeField]
+        private PlayerController _playerController = null;
+
+        //[SerializeField]
+        //private float _force = 1f;
+
+
+        private void OnEnable()
+        {
+            if (_inputActionmap.TryFindAction("BasicAttack", out InputAction _inputaction) == true)
+            {
+                _inputaction.performed -= PlayerControllerBasicAttackPerformed;
+                _inputaction.performed += PlayerControllerBasicAttackPerformed;
+            }
+            _inputaction.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _inputaction.performed -= PlayerControllerBasicAttackPerformed;
+            _inputaction.Disable();
+        }
+
+
+        private void Update()
+        {
+            if (_starttimer == true)
+            {
+                _bigfloat -= Time.deltaTime;
+
+            }
+
+            if (_bigfloat < 0)
+            {
+                _fluteattackPrefab.SetActive(false);
+                _starttimer = false;
+                _bigfloat = 0.2f;
+                _startdown = true;
+            }
+
+            if(_startdown == true)
+            {
+                _cooldown -= Time.deltaTime;
+            }
+
+            if (_cooldown < 0)
+            {
+                _startdown = false;
+                _cooldown = 1f;
+            }
+        }
+
+        private void PlayerControllerBasicAttackPerformed(/*PlayerController sender,*/ InputAction.CallbackContext obj)
+        {
+           
+            if (_startdown == false)
+            {
+                _fluteattackPrefab.SetActive(true);
+                _starttimer = true;
+            }
+          
+        }
+
+    
+    }
+}
---
+++ 2DPlatformer_Test/Assets/2DPlatformer/Scripts/Exercices/FluteAttackObject.cs
@@ -0,0 +1,20 @@
+namespace GSGD2.Gameplay
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using GSGD2.Utilities;
+
+    public class FluteAttackObject : MonoBehaviour
+    {
+
+      
+
+
+
+       
+
+        
+    }
+
+}
