a038c77eea037ae744d22c7ada891a2c5ba1d154
Widocool <hugo.wido@gmail.com>
2022-02-01 13:52:19 +0100

    add MainMapLowMetrics.unity

--- 2DPlatformer_Test/Assets/2DPlatformer/Scripts/UI/Shop/Spot.cs
+++ 2DPlatformer_Test/Assets/2DPlatformer/Scripts/UI/Shop/Spot.cs
@@ -4,17 +4,23 @@ namespace GSGD2.Gameplay
     using System.Collections.Generic;
     using UnityEngine;
     using GSGD2.Player;
+    using UnityEngine.EventSystems;
 
     public class Spot : MonoBehaviour
     {
         [SerializeField]
         private ShopUi _shopui = null;
 
+        [SerializeField]
+        private GameObject _buttonFocus = null;
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.GetComponentInParent<PlayerReferences>() == true)
             {
                 _shopui.gameObject.SetActive(true);
+                EventSystem.current.SetSelectedGameObject(_buttonFocus.gameObject);
+
             }
         }
 
