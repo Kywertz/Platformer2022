1f7bd72bbdaad9f518bcd67a554c9083f301a5dd
Widocool <hugo.wido@gmail.com>
2022-02-02 11:53:59 +0100

    WidoVersion

---
+++ 2DPlatformer_Test/Assets/2DPlatformer/Scripts/Gameplay/Weapon/SecondAttack.cs
@@ -0,0 +1,53 @@
+namespace GSGD2.Gameplay
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using GSGD2.Player;
+    using UnityEngine.InputSystem;
+
+    public class SecondAttack : MonoBehaviour
+    {
+        [SerializeField]
+        Projectile _rattolunch = null;
+
+        [SerializeField]
+        GameObject _offset = null;
+
+        [SerializeField]
+        private InputActionMapWrapper _inputMapWrapperAttack = null;
+
+        private InputAction _inputaction = null;
+
+        private void OnEnable()
+        {
+            if (_inputMapWrapperAttack.TryFindAction("SeconAttackInput", out _inputaction) == true)
+            {
+                _inputaction.performed -= SecondAttack_Performed;
+                _inputaction.performed += SecondAttack_Performed;
+            }
+            _inputaction.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _inputaction.performed -= SecondAttack_Performed;
+            _inputaction.Disable();
+        }
+
+        private void SecondAttack_Performed(InputAction.CallbackContext obj)
+        {
+
+            Shoot();
+        }
+
+
+        public void Shoot()
+        {
+            Instantiate(_rattolunch, _offset.transform.position, transform.rotation);
+        }
+
+       
+    }
+
+}
